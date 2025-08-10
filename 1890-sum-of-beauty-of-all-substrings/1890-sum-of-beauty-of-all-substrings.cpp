72805fee79cf3cda97fdef90f0d99f9c424bcf98
Mayank Rajput <119316016+mayankrajput123@users.noreply.github.com>
2025-08-10 18:37:23 +0530

    Time: 131 ms (23.12%), Space: 26 MB (35.52%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.size();
+        int total=0;
+        for(int i =0; i<n; i++){
+            unordered_map<char,int> arr;
+            for(int j =i; j<n; j++){
+                arr[s[j]]++;
+                int maxi=0;
+                int mini=n+1;
+                for(unordered_map<char,int>::iterator it=arr.begin(); it!=arr.end(); it++){
+                    int c= it->second;
+                    maxi=max(maxi,c);
+                    mini=min(mini,c);
+                }
+                total+=(maxi-mini);
+            }
+        }
+        return total;
+    }
+};

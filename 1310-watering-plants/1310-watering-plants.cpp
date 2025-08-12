75575ab4ec4175c0aed91b7136a3cb4eafca8410
Mayank Rajput <119316016+mayankrajput123@users.noreply.github.com>
2025-08-13 02:11:51 +0530

    Time: 0 ms (100%), Space: 11.9 MB (88.45%) - LeetHub

--- 1310-watering-plants/1310-watering-plants.cpp
+++ 1310-watering-plants/1310-watering-plants.cpp
@@ -3,13 +3,13 @@ public:
     int wateringPlants(vector<int>& plants, int capacity) {
         int n = plants.size();
         int c=0;
-        int currwater=capacity;
+        int cw=capacity;
         for(int i =0; i<n; i++){
-            if(currwater<plants[i]){
+            if(cw<plants[i]){
                 c+=(i*2);
-                currwater=capacity;
+                cw=capacity;
             }
-            currwater-=plants[i];
+            cw-=plants[i];
             c+=1;
         }
         return c;

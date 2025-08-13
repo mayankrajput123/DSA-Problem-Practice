aff4c9c2f50e68e0ff7dc40601f0d357fd620db3
Mayank Rajput <119316016+mayankrajput123@users.noreply.github.com>
2025-08-13 12:51:45 +0530

    Time: 4 ms (47.44%), Space: 9.1 MB (14.61%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0){
+            return false;
+        }
+        while(n>0){
+            if(n==1){
+                return true;
+            }
+            if(n%3 != 0){
+                break;
+            }
+            n = n/3;
+        }
+         return false;
+    }
+};
